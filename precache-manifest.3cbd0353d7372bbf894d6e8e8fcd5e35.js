df4f938c3c62d2aae5df9e6ac5f3feb5033d7bb8
Ivan <komjenic@gmail.com>
2019-11-03 15:51:43 +0100

    Updates

---
+++ precache-manifest.3cbd0353d7372bbf894d6e8e8fcd5e35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f51a4fce156120b94bf33f5cd53a48a",
+    "url": "/ivan-travel1/index.html"
+  },
+  {
+    "revision": "7800bba2abe14472813d",
+    "url": "/ivan-travel1/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9cfa9bd803833ce6bf84",
+    "url": "/ivan-travel1/static/js/2.731f73a5.chunk.js"
+  },
+  {
+    "revision": "7800bba2abe14472813d",
+    "url": "/ivan-travel1/static/js/main.ec327010.chunk.js"
+  },
+  {
+    "revision": "815c2e1b8d4c9de70274",
+    "url": "/ivan-travel1/static/js/runtime-main.5d57ff02.js"
+  }
+]);
